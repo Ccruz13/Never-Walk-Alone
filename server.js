471577ddefcc4fe256d14f3621db415ee6433886
Ccruz13 <88520938+Ccruz13@users.noreply.github.com>
2021-12-05 20:50:35 -0500

    updates views folder

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
     secret: 'Super secret secret',
     cookie: { expires: 10 * 60 * 1000 },
-    resave: false,
+    resave: true,
+    rolling: true,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
